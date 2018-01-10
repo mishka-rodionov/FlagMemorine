448e869c1811d8264290bfea7e71f6440a24b49a
Mikhail Rodionov <mishka_rodionov@mail.ru>
2018-01-10 22:45:47 +0300

    add fill array

--- app/src/main/java/com/example/mishka/flagmemorine/MainActivity.java
+++ app/src/main/java/com/example/mishka/flagmemorine/MainActivity.java
@@ -15,8 +15,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -27,6 +29,7 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         //************************
         relativeLayout = (RelativeLayout) findViewById(R.id.relativelayout);
+        initFlags();
         //************************
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -93,6 +96,7 @@ public class MainActivity extends AppCompatActivity
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
             View view  = getLayoutInflater().inflate(R.layout.layout_6_6, null);
+            final TextView result = (TextView) view.findViewById(R.id.result);
             relativeLayout.addView(view);
             View.OnClickListener onClickListener = new View.OnClickListener() {
                 @Override
@@ -100,111 +104,147 @@ public class MainActivity extends AppCompatActivity
                     switch(view.getId()){
                         case R.id.imageButton2:
                             Log.d(LOG_TAG, "click imageButton" + view.getTag());
+                            result.setText(indexCalc(view.getTag().toString()));
                             break;
                         case R.id.imageButton3:
                             Log.d(LOG_TAG, "click imageButton" + view.getTag());
+                            result.setText(indexCalc(view.getTag().toString()));
                             break;
                         case R.id.imageButton4:
                             Log.d(LOG_TAG, "click imageButton" + view.getTag());
+                            result.setText(indexCalc(view.getTag().toString()));
                             break;
                         case R.id.imageButton5:
                             Log.d(LOG_TAG, "click imageButton" + view.getId());
+                            result.setText(indexCalc(view.getTag().toString()));
                             break;
                         case R.id.imageButton6:
                             Log.d(LOG_TAG, "click imageButton" + view.getId());
+                            result.setText(indexCalc(view.getTag().toString()));
                             break;
                         case R.id.imageButton7:
                             Log.d(LOG_TAG, "click imageButton" + view.getId());
+                            result.setText(indexCalc(view.getTag().toString()));
                             break;
                         case R.id.imageButton8:
                             Log.d(LOG_TAG, "click imageButton" + view.getId());
+                            result.setText(indexCalc(view.getTag().toString()));
                             break;
                         case R.id.imageButton9:
                             Log.d(LOG_TAG, "click imageButton" + view.getId());
+                            result.setText(indexCalc(view.getTag().toString()));
                             break;
                         case R.id.imageButton10:
                             Log.d(LOG_TAG, "click imageButton" + view.getId());
+                            result.setText(indexCalc(view.getTag().toString()));
                             break;
                         case R.id.imageButton11:
                             Log.d(LOG_TAG, "click imageButton" + view.getId());
+                            result.setText(indexCalc(view.getTag().toString()));
                             break;
                         case R.id.imageButton12:
                             Log.d(LOG_TAG, "click imageButton" + view.getId());
+                            result.setText(indexCalc(view.getTag().toString()));
                             break;
                         case R.id.imageButton13:
                             Log.d(LOG_TAG, "click imageButton" + view.getId());
+                            result.setText(indexCalc(view.getTag().toString()));
                             break;
                         case R.id.imageButton14:
                             Log.d(LOG_TAG, "click imageButton" + view.getId());
+                            result.setText(indexCalc(view.getTag().toString()));
                             break;
                         case R.id.imageButton15:
                             Log.d(LOG_TAG, "click imageButton" + view.getId());
+                            result.setText(indexCalc(view.getTag().toString()));
                             break;
                         case R.id.imageButton16:
                             Log.d(LOG_TAG, "click imageButton" + view.getId());
+                            result.setText(indexCalc(view.getTag().toString()));
                             break;
                         case R.id.imageButton17:
                             Log.d(LOG_TAG, "click imageButton" + view.getId());
+                            result.setText(indexCalc(view.getTag().toString()));
                             break;
                         case R.id.imageButton18:
                             Log.d(LOG_TAG, "click imageButton" + view.getId());
+                            result.setText(indexCalc(view.getTag().toString()));
                             break;
                         case R.id.imageButton19:
                             Log.d(LOG_TAG, "click imageButton" + view.getId());
+                            result.setText(indexCalc(view.getTag().toString()));
                             break;
                         case R.id.imageButton20:
                             Log.d(LOG_TAG, "click imageButton" + view.getId());
+                            result.setText(indexCalc(view.getTag().toString()));
                             break;
                         case R.id.imageButton21:
                             Log.d(LOG_TAG, "click imageButton" + view.getId());
+                            result.setText(indexCalc(view.getTag().toString()));
                             break;
                         case R.id.imageButton22:
                             Log.d(LOG_TAG, "click imageButton" + view.getId());
+                            result.setText(indexCalc(view.getTag().toString()));
                             break;
                         case R.id.imageButton23:
                             Log.d(LOG_TAG, "click imageButton" + view.getId());
+                            result.setText(indexCalc(view.getTag().toString()));
                             break;
                         case R.id.imageButton24:
                             Log.d(LOG_TAG, "click imageButton" + view.getId());
+                            result.setText(indexCalc(view.getTag().toString()));
                             break;
                         case R.id.imageButton25:
                             Log.d(LOG_TAG, "click imageButton" + view.getId());
+                            result.setText(indexCalc(view.getTag().toString()));
                             break;
                         case R.id.imageButton26:
                             Log.d(LOG_TAG, "click imageButton" + view.getId());
+                            result.setText(indexCalc(view.getTag().toString()));
                             break;
                         case R.id.imageButton27:
                             Log.d(LOG_TAG, "click imageButton" + view.getId());
+                            result.setText(indexCalc(view.getTag().toString()));
                             break;
                         case R.id.imageButton28:
                             Log.d(LOG_TAG, "click imageButton" + view.getId());
+                            result.setText(indexCalc(view.getTag().toString()));
                             break;
                         case R.id.imageButton29:
                             Log.d(LOG_TAG, "click imageButton" + view.getId());
+                            result.setText(indexCalc(view.getTag().toString()));
                             break;
                         case R.id.imageButton30:
                             Log.d(LOG_TAG, "click imageButton" + view.getId());
+                            result.setText(indexCalc(view.getTag().toString()));
                             break;
                         case R.id.imageButton31:
                             Log.d(LOG_TAG, "click imageButton" + view.getId());
+                            result.setText(indexCalc(view.getTag().toString()));
                             break;
                         case R.id.imageButton32:
                             Log.d(LOG_TAG, "click imageButton" + view.getId());
+                            result.setText(indexCalc(view.getTag().toString()));
                             break;
                         case R.id.imageButton33:
                             Log.d(LOG_TAG, "click imageButton" + view.getId());
+                            result.setText(indexCalc(view.getTag().toString()));
                             break;
                         case R.id.imageButton34:
                             Log.d(LOG_TAG, "click imageButton" + view.getId());
+                            result.setText(indexCalc(view.getTag().toString()));
                             break;
                         case R.id.imageButton35:
                             Log.d(LOG_TAG, "click imageButton" + view.getId());
+                            result.setText(indexCalc(view.getTag().toString()));
                             break;
                         case R.id.imageButton36:
                             Log.d(LOG_TAG, "click imageButton" + view.getId());
+                            result.setText(indexCalc(view.getTag().toString()));
                             break;
                         case R.id.imageButton37:
                             Log.d(LOG_TAG, "click imageButton" + view.getId());
+                            result.setText(indexCalc(view.getTag().toString()));
                             break;
                     }
                 }
@@ -271,8 +311,33 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
+    public String indexCalc(String imageButtonTag){
+        int tag = Integer.parseInt(imageButtonTag);
+        if(tag < 6){
+            return getFlag(tag/6, tag);
+        } else {
+            return getFlag(tag/6, tag%6);
+        }
+    }
+
+    public String getFlag(int i, int j){
+        return flags[i][j];
+    }
+
+    public void initFlags(){
+        int count = 0;
+        Random rnd = new Random();
+        int rand = rnd.nextInt(36);
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                flags[i][j] = "" + rand;
+            }
+        }
+    }
+
     private RelativeLayout relativeLayout;
     private String LOG_TAG = "flagmemorine";
     private ArrayList<ImageButton> imageButtonArrayList;
     private int capacity6x6 = 36;
+    private String[][] flags = new String[6][6];
 }
