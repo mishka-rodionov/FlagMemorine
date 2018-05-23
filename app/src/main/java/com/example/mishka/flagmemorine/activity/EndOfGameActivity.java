732e4cc26ffeb2fb93a71a40d2255d5f79329dd1
Mikhail Rodionov <rodionov.mikhail.a@gmail.com>
2018-05-23 16:54:22 +0400

    add class facts

--- app/src/main/java/com/example/mishka/flagmemorine/activity/EndOfGameActivity.java
+++ app/src/main/java/com/example/mishka/flagmemorine/activity/EndOfGameActivity.java
@@ -12,6 +12,7 @@ import android.widget.TextView;
 
 import com.example.mishka.flagmemorine.R;
 import com.example.mishka.flagmemorine.logic.Data;
+import com.example.mishka.flagmemorine.logic.Facts;
 
 public class EndOfGameActivity extends AppCompatActivity {
 
@@ -37,6 +38,7 @@ public class EndOfGameActivity extends AppCompatActivity {
         step = (TextView) findViewById(R.id.stepEndOfGame);
         time = (TextView) findViewById(R.id.timeEndOfGame);
         message = (TextView) findViewById(R.id.message);
+        factsTextView = (TextView) findViewById(R.id.factsTextView);
 
         if (activityName.equals("RoomBattlefield")){
             result = getIntent().getStringExtra("result");
@@ -66,6 +68,7 @@ public class EndOfGameActivity extends AppCompatActivity {
         score.setText(score.getText().toString() + " " + scoreValue);
         step.setText(step.getText().toString() + " " + stepValue);
         time.setText(time.getText().toString() + " " + timeValue);
+        factsTextView.setText(Facts.randomCountryFacts());
 
 
         View.OnClickListener onClickListener = new View.OnClickListener() {
@@ -131,6 +134,7 @@ public class EndOfGameActivity extends AppCompatActivity {
     private TextView step;
     private TextView time;
     private TextView message;
+    private TextView factsTextView;
     private android.support.v7.widget.Toolbar endOfGameToolbar;
 
     private ImageButton restart;
---
+++ app/src/main/java/com/example/mishka/flagmemorine/logic/Facts.java
@@ -0,0 +1,22 @@
+package com.example.mishka.flagmemorine.logic;
+
+import java.util.Random;
+
+/**
+ * Created by Lab1 on 23.05.2018.
+ */
+
+public class Facts {
+
+    public static String randomCountryFacts(){
+        Random rnd = new Random();
+        int index = rnd.nextInt(countryFacts.length);
+        return countryFacts[index];
+    }
+
+    private static String[] countryFacts = {
+            "Russia - is a biggest country of the world.",
+            "Moscow - is a capital of Russian Federation.",
+            "Baikal - is  a deepest lake of the world."
+    };
+}
